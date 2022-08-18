0a6749b84c74ffa0fedccd80d2010e0618145cb4
Matheus <matheusjesus885@outlook.com>
2022-08-18 16:21:26 -0300

    Primeiros passos js switch case

---
+++ Fundamentos/Bloco04-JavaScript/Dia01-introducao-javaScript/scripts5.js
@@ -0,0 +1,18 @@
+let statusCandidato = "reprovado";
+
+switch (statusCandidato) {
+    case "aprovado":
+        console.log("Siga");
+        break;
+
+    case "lista":
+        console.log("atenção");
+        break;
+
+    case "reprovado":
+        console.log("pare");
+        break;
+
+        default: 
+        console.log("não se aplica!")
+}
