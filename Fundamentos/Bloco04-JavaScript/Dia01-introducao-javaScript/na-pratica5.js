217614631543e04f65524bf8b2ee8ec2f583980a
Matheus <matheusjesus885@outlook.com>
2022-08-18 19:51:04 -0300

    parte 5 ok

---
+++ Fundamentos/Bloco04-JavaScript/Dia01-introducao-javaScript/na-pratica5.js
@@ -0,0 +1,13 @@
+let areaTriangulo1 = 0;
+let areaTriangulo2 = 81;
+let areaTriangulo3 = 100;
+let anguloTriangulo = areaTriangulo1 + areaTriangulo2 + areaTriangulo3;
+let triangulo;
+if (anguloTriangulo == 180) {
+    console.log('é triangulo?',triangulo = true);
+}
+
+else {
+    console.log('é triangulo?', triangulo = false);
+}
+
