efd9ef97fc70ae13763772460f8d3a09d03ededf
Matheus <matheusjesus885@outlook.com>
2022-08-19 00:19:32 -0300

    parte 8 ok

---
+++ Fundamentos/Bloco04-JavaScript/Dia01-introducao-javaScript/no-pratica8.js
@@ -0,0 +1,15 @@
+const num1 = 15;
+const num2 = 17;
+const num3 = 15;
+
+let resultado;
+
+if (num1%2 == 0 || num2%2 == 0 || num3%2 == 0) {
+    resultado = true;
+    console.log(resultado)
+}
+
+else {
+    resultado = false;
+    console.log(resultado)
+}
