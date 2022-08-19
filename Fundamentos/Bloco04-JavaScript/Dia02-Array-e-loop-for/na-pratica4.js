19b8bcbcdc1fad67d2aa64352dd21f4a7ede360c
Matheus <matheusjesus885@outlook.com>
2022-08-19 18:25:31 -0300

    na pratica 4 ok

---
+++ Fundamentos/Bloco04-JavaScript/Dia02-Array-e-loop-for/na-pratica4.js
@@ -0,0 +1,18 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado =0;
+let media;
+
+for (index = 0; index < numbers.length; index += 1) {
+    resultado = (numbers[index]) + resultado
+    }
+ 
+media = resultado / (numbers.length)    
+console.log(media)
+
+    if (media > 20) {
+        console.log('valor maior que 20')
+    }
+
+    else {
+        console.log('valor menor ou igual a 20')
+    }
