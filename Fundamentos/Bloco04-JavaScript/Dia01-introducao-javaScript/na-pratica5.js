fd98c8c4455c0cbdb2d13902122d0171b621f18c
Matheus <matheusjesus885@outlook.com>
2022-08-18 23:18:58 -0300

    ajuste pt5

--- Fundamentos/Bloco04-JavaScript/Dia01-introducao-javaScript/na-pratica5.js
+++ Fundamentos/Bloco04-JavaScript/Dia01-introducao-javaScript/na-pratica5.js
@@ -1,9 +1,9 @@
-let areaTriangulo1 = 0;
-let areaTriangulo2 = 81;
+let areaTriangulo1 = 20;
+let areaTriangulo2 = 60;
 let areaTriangulo3 = 100;
 let anguloTriangulo = areaTriangulo1 + areaTriangulo2 + areaTriangulo3;
 let triangulo;
-if (anguloTriangulo == 180) {
+if (anguloTriangulo == 180 && areaTriangulo1 > 0 && areaTriangulo2 > 0 && areaTriangulo3) {
     console.log('é triangulo?',triangulo = true);
 }
 
