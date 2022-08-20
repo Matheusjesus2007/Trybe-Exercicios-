3916c4c11951c74a38109c2c81fc427a22e769a8
Matheus <matheusjesus885@outlook.com>
2022-08-20 14:42:12 -0300

    salario liquido

---
+++ Fundamentos/Bloco04-JavaScript/Dia01-introducao-javaScript/salario-líquido.js
@@ -0,0 +1,75 @@
+let salarioBruto = 2200;
+let aliquotaInss;
+let aliquotaInssMax = 570.88;
+let aliquotaIr;
+let salarioBase;
+let salarioLiquido;
+let descontoIr;
+let deduzir;
+
+ if (salarioBruto <= 1556.94) {
+    aliquotaInss = 8;
+ } 
+
+ else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+    aliquotaInss = 9;
+ }   
+
+ else if (salarioBruto >= 2549.93 && salarioBruto <= 5189.82) {
+    aliquotaInss = 11;
+ } 
+
+ else {
+   aliquotaInss = aliquotaInssMax
+ };
+
+
+ if (salarioBruto > 5189.82) {
+ salarioBase = salarioBruto - aliquotaInssMax; 
+ }
+
+else {
+    salarioBase = salarioBruto - ((aliquotaInss/100) * salarioBruto);  
+}
+
+
+if (salarioBase <= 1903.98) {
+    aliquotaIr = 0;
+}
+
+else if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
+    aliquotaIr = 7.5;
+    deduzir = 142.80
+ }   
+
+ else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
+    aliquotaIr = 15;
+    deduzir = 354.80;
+ } 
+
+ else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+    aliquotaIr = 22.5;
+    deduzir = 636.13
+
+ }
+
+ else {
+    aliquotaIr = 27.5
+    deduzir = 869.36
+ }
+    
+   
+ 
+
+
+
+
+
+ 
+ descontoIr = (((aliquotaIr/100) * salarioBase) - deduzir);
+ salarioLiquido = (salarioBase - descontoIr);
+
+
+
+console.log(salarioBruto)
+console.log(salarioLiquido)
