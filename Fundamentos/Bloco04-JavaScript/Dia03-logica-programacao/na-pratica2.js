fd64b3e4e9d3722eced50fb68672336686193586
Matheus <matheusjesus885@outlook.com>
2022-08-22 18:32:05 -0300

    na-pratica 2 ok

---
+++ Fundamentos/Bloco04-JavaScript/Dia03-logica-programacao/na-pratica2.js
@@ -0,0 +1,24 @@
+/*l let word = 'Matheus';
+let newWord = '';
+
+for (index = 0; index < word.length; index +=1) {
+   newWord += word[index]
+
+
+}  
+console.log(newWord) 
+*/
+
+
+let word = 'Matheus';
+let newWord = '';
+
+for (index = word.length - 1; index >= 0; index-=1) {
+   newWord += word[index]
+
+
+
+}
+
+
+console.log(newWord) 
