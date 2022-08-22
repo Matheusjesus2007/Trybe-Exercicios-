95a3276f14528fe293d2bd8ce17a49f840265b58
Matheus <matheusjesus885@outlook.com>
2022-08-22 17:29:16 -0300

    na pratica 1 ok

---
+++ Fundamentos/Bloco04-JavaScript/Dia03-logica-programacao/na-pratica.js
@@ -0,0 +1,12 @@
+let fatorial = 10;
+let antecessor = fatorial;
+let resultado = fatorial;
+
+
+for (index = 1; index < fatorial; index += 1){
+   antecessor -= 1;  
+   resultado = resultado * antecessor;
+     }
+    
+   console.log(resultado)
+
