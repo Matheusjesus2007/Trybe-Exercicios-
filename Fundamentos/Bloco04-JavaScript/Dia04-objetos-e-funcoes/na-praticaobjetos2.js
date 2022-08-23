69228df534d3c326e5fb87fb240b4fea28d4990f
Matheus <matheusjesus885@outlook.com>
2022-08-23 18:12:56 -0300

    na pratica objetos e for in 2 ok

---
+++ Fundamentos/Bloco04-JavaScript/Dia04-objetos-e-funcoes/na-praticaobjetos2.js
@@ -0,0 +1,11 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+
+  let propriedade = 'recorrente'
+
+  info[propriedade] = 'Sim';
+  console.log(info)
