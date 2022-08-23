f6b6134eef00e9b31f57e53ddfa618e211f7a556
Matheus <matheusjesus885@outlook.com>
2022-08-23 19:33:45 -0300

    na pratica objetos e for in 8 ok

--- Fundamentos/Bloco04-JavaScript/Dia04-objetos-e-funcoes/na-praticaobjetos7.js
+++ Fundamentos/Bloco04-JavaScript/Dia04-objetos-e-funcoes/na-praticaobjetos7.js
@@ -16,9 +16,9 @@ let leitor = {
     ],
   };
 
-  console.log('O livro favorito de ', leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo) 
+  console.log(leitor.livrosFavoritos, 'Julia tem 2 livros favoritos') 
 
-  console.log(leitor.livrosFavoritos)
+
 
 
 
