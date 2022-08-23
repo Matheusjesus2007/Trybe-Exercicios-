1858eaf0647ab243d6924461d26c66cd21ce5986
Matheus de Jesus <matheusjesus885@outlook.com>
2022-08-22 23:41:51 -0300

    na-pratica 4 not ok

---
+++ Fundamentos/Bloco04-JavaScript/Dia03-logica-programacao/na-pratica4.js
@@ -0,0 +1,27 @@
+/* 4- Um número primo é um número inteiro maior do que 1 que possui somente dois divisores, ou seja, é divisível por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 2 e 50. */
+
+let numeros = [13,14];
+let numeroAtual = 0;
+let maiorPrimo = 0;
+let divisores =0;
+
+for (index = 1; index <= numeros[0]; index += 1 ) {
+        if (curioso = numeros[0] % index == 0) {
+        divisores +=1;  
+         
+    }
+    console.log(curioso) 
+    }
+
+    
+    
+    console.log(divisores)
+
+
+
+
+
+
+
+
+   
