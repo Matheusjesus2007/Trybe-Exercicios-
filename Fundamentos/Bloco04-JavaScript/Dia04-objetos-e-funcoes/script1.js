8242b5f585cf05f6878c7cb8665312ae6222d758
Matheus <matheusjesus885@outlook.com>
2022-08-23 15:44:05 -0300

    Fixar funcoes ok

---
+++ Fundamentos/Bloco04-JavaScript/Dia04-objetos-e-funcoes/script1.js
@@ -0,0 +1,19 @@
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
+
+  for (let pessoas in names){
+    console.log(names[pessoas])
+  }
+
+
+  let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+  for (let carro in car)
+  console.log(carro, car[carro] )
