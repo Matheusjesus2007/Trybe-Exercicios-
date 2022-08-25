b339e3ee69b524abdfabc4f0602c062baa611b6b
Matheus <matheusjesus885@outlook.com>
2022-08-25 15:50:41 -0300

    parte 3.5 ok

--- Fundamentos/Bloco05-Dom-Eventos-WebStorage/Dia01-Dom-e-Seletores/script.js
+++ Fundamentos/Bloco05-Dom-Eventos-WebStorage/Dia01-Dom-e-Seletores/script.js
@@ -1,11 +1,11 @@
-document.getElementById('page-title').innerText = 'Prision Break';
-document.getElementById('second-paragraph').innerText = 'essa é a historia de um homem que foi para a prisão regastar o irmão';
-document.getElementById('subtitle').innerText = 'Hoje na bandTV às 21:00H';
-let subtitulo = document.getElementById('subtitle');
+document.querySelector('h2').innerText = 'Prision Break';
+document.querySelectorAll('p')[1].innerText = 'essa é a historia de um homem que foi para a prisão regastar o irmão';
+document.querySelector('h4').innerText = 'Hoje na bandTV às 21:00H';
+let subtitulo = document.querySelector('h4');
 subtitulo.style.color = 'green';
 
-let paragrafo = document.getElementsByClassName('paragrafo')[0];
+let paragrafo = document.querySelectorAll('p')[0];
 paragrafo.style.color = 'blue';
 
-subtitulo1 = document.getElementsByTagName('h4')[0];
+subtitulo1 = document.querySelectorAll('h4')[0];
 subtitulo1.style.color = 'orange';
