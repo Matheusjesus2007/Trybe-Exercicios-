97d3f207f94d4fa48ec0734415d4804dad787dc6
Matheus <matheusjesus885@outlook.com>
2022-08-25 15:10:35 -0300

    iniciando

---
+++ Fundamentos/Bloco05-Dom-Eventos-WebStorage/Dia01-Dom-e-Seletores/script.js
@@ -0,0 +1,3 @@
+document.getElementById('page-title').innerText = 'Prision Break'
+document.getElementById('second-paragraph').innerText = 'essa é a historia de um homem que foi para a prisão regastar o irmão'
+document.getElementById('subtitle').innerText = 'Hoje na bandTV às 21:00H'
