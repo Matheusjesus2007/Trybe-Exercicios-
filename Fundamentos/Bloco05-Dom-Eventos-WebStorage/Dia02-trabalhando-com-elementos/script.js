68b5ebdacf9cf0a34f021df2244f854415d116ea
Matheus <matheusjesus885@outlook.com>
2022-08-26 18:44:20 -0300

    criando elementos ok

--- Fundamentos/Bloco05-Dom-Eventos-WebStorage/Dia02-trabalhando-com-elementos/script.js
+++ Fundamentos/Bloco05-Dom-Eventos-WebStorage/Dia02-trabalhando-com-elementos/script.js
@@ -1,10 +1,23 @@
 /* Crie um irmão para elementoOndeVoceEsta. */
 
-
+/* onde eu quero adicionar ? */
 const elementoPai = document.getElementById('pai');
 
+/* criando um elento! */
 const newElement = document.createElement('section')
 
-newElement.id = '#irmaoElementoOndeVoceEsta';
+/* adicionar class ou id ao elemento criado */
+newElement.id = 'irmaoElementoOndeVoceEsta';
 
+/* adicionando o elemento criado ao arquivo HTMl */
 elementoPai.appendChild(newElement);
+
+const filhoElementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+const newElement2 = document.createElement('section')
+newElement2.id = 'filhoElementoOndeVoceEsta'
+filhoElementoOndeVoceEsta.appendChild(newElement2)
+
+const filhoPrimeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
+const newElement3 = document.createElement('section')
+newElement3.id = 'filhoPrimeiroFilhoDoFilho'
+filhoPrimeiroFilhoDoFilho.appendChild(newElement3)
