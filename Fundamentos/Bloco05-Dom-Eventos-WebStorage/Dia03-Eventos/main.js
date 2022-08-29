0c8eac5545d0510d7d82f3505c6ca1b247417b69
Matheus <matheusjesus885@outlook.com>
2022-08-29 16:01:28 -0300

    eventos 4 ok

--- Fundamentos/Bloco05-Dom-Eventos-WebStorage/Dia03-Eventos/main.js
+++ Fundamentos/Bloco05-Dom-Eventos-WebStorage/Dia03-Eventos/main.js
@@ -29,9 +29,16 @@ function alteraText (evento2) {
     
 }
 
+
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
+myWebpage.addEventListener('dblclick', myPorti)
+
+function myPorti (evento3) {
+ window.open('https://github.com/Matheusjesus2007/Trybe-Exercicios-');
+}
+
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
