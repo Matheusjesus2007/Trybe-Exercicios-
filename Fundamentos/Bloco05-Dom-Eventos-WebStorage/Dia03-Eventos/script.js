606365ec3dbd3ef2b0e2037a1258281b148934ff
Matheus <matheusjesus885@outlook.com>
2022-08-29 19:10:25 -0300

    Na pratica 1 ok

---
+++ Fundamentos/Bloco05-Dom-Eventos-WebStorage/Dia03-Eventos/script.js
@@ -0,0 +1,33 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+  createDaysOfTheWeek();
+  
+  /* let ulDias = document.getElementsByTagName('ul')
+  ulDias.id = "days" */
+
+  let mes = document.getElementById('days')
+  let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    
+   for(index = 2; index < decemberDaysList.length; index += 1) {
+   let dias = document.createElement('li')
+    dias.className = 'days';
+    if (decemberDaysList[index] === 24 || decemberDaysList[index] === 25 || decemberDaysList[index] === 31) {
+    dias.className += ' holiday'    
+    }
+    if (decemberDaysList[index] === 4 || decemberDaysList[index] === 11 || decemberDaysList[index] === 18 || decemberDaysList[index] === 25) {
+    dias.className += ' friday'    
+        }
+    dias.innerText = decemberDaysList[index]
+    mes.appendChild(dias)  
+  }
