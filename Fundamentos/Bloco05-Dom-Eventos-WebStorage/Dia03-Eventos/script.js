c893605513e8d8dfe0832aa8614d96d0467eb59a
Matheus de Jesus <matheusjesus885@outlook.com>
2022-08-29 23:54:07 -0300

    Exercicio 5 ok

--- Fundamentos/Bloco05-Dom-Eventos-WebStorage/Dia03-Eventos/script.js
+++ Fundamentos/Bloco05-Dom-Eventos-WebStorage/Dia03-Eventos/script.js
@@ -16,7 +16,7 @@ function createDaysOfTheWeek() {
   let mes = document.getElementById('days')
   let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
     
-   for(index = 2; index < decemberDaysList.length; index += 1) {
+   for(index = 0; index < decemberDaysList.length; index += 1) {
    let dias = document.createElement('li')
     dias.className = 'days';
     if (decemberDaysList[index] === 24 || decemberDaysList[index] === 25 || decemberDaysList[index] === 31) {
@@ -56,5 +56,18 @@ function buttonSexta (entrada1) {
   buttonSextas.id = 'btn-friday';
   buttonSextas.innerText = entrada1;
   divButton.appendChild(buttonSextas);
+  buttonSextas.addEventListener('click', txtSexta)
 }
 buttonSexta('Sexta-feira')
+
+function txtSexta () {
+  let sextasoriginais = [4, 11, 18, 25];
+  let classeSexta = document.querySelectorAll('.friday');
+  for (index = 0; index < classeSexta.length; index += 1) {
+  if (classeSexta[index].innerText !== 'Sexta-feira') {
+     classeSexta[index].innerText = 'Sexta-feira';
+    } else { classeSexta[index].innerText = sextasoriginais[index];
+
+    }
+  }
+}
