3bc0ac61a1e5664ff331f88f4e9a18dc80959a21
Matheus <matheusjesus885@outlook.com>
2022-09-15 19:34:19 -0300

    pt2 exercicio 4.2 ok

--- Fundamentos/Secao07-JSES6-Teste-unitarios/Dia01-Letcons-arrowfunctions/script4.js
+++ Fundamentos/Secao07-JSES6-Teste-unitarios/Dia01-Letcons-arrowfunctions/script4.js
@@ -1,8 +1,15 @@
 function substituaX (nome) {
     let frase = 'Tryber x aqui!'
     frase = frase.replace('x', nome)
-    console.log(frase)
+    minhasSkills(frase)
+    
 }
 
 substituaX('Matheus');
 substituaX('Rafael');
+
+function minhasSkills(frase) {
+    const skills = ['CSS', 'HTML', 'JS'];
+    let resultado = `${frase} Minhas três principais habilidades são: ${skills}`
+    console.log(resultado)
+}
